1ae468f96ffe4a5caf5ac644457bb7e3f717bf7b
leejuhanKr <juhan96@gmail.com>
2022-05-28 00:12:37 +0900

    coordinate 표기 추가

--- coordinates/main.js
+++ coordinates/main.js
@@ -1,10 +1,13 @@
 const targetMark = document.querySelector('.target_ground')
 const target = document.querySelector('.target_wrapper')
+const coordinate = document.querySelector('.tag')
 
 
 targetMark.addEventListener('mousemove', event  =>{
-    console.log(event.clientX, event.clientY)
-    target.style.left = `${event.clientX}px`
-    target.style.top = `${event.clientY}px`
+    // console.log(event.clientX, event.clientY)
+    const [x,y] = [event.clientX, event.clientY]
+    target.style.left = `${x}px`
+    target.style.top = `${y}px`
+    coordinate.textContent = `${x}px, ${y}px`
 })
 
