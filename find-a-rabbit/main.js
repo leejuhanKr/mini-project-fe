1d352787bec750f12d87b092af4e85886be5ca6d
leejuhanKr <juhan96@gmail.com>
2022-05-31 22:33:45 +0900

    button 클릭시 rabbit이 화면 가운데로 오도록 정렬

--- find-a-rabbit/main.js
+++ find-a-rabbit/main.js
@@ -3,7 +3,10 @@ const findBtn = document.querySelector('.find-Btn')
 
 findBtn.addEventListener('click', () => {
   const rabbitRect = rabbit.getBoundingClientRect()
-  const [x,y] = [rabbitRect.left,rabbitRect.top]
-  console.log(x,y)
-  window.scrollTo(x, y);
+  const [x,y] = [rabbitRect.left, rabbitRect.top]
+  const windowHeight = window.innerHeight;
+
+  const [targetX, targetY] = [0, parseInt(y - (windowHeight/2) + (rabbit.height)/2)]
+  console.log(targetX, targetY)
+  window.scrollTo(targetX, targetY)
 })
