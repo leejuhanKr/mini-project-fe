88378faa7baae9e558ac61288f6750e628183f3c
leejuhanKr <juhan96@gmail.com>
2022-05-31 22:56:18 +0900

    스크롤 이동 로직 개선

--- find-a-rabbit/main.js
+++ find-a-rabbit/main.js
@@ -2,11 +2,19 @@ const rabbit = document.querySelector('.rabbit')
 const findBtn = document.querySelector('.find-Btn')
 
 findBtn.addEventListener('click', () => {
-  const rabbitRect = rabbit.getBoundingClientRect()
-  const [x,y] = [rabbitRect.left, rabbitRect.top]
-  const windowHeight = window.innerHeight;
+  // 기존 방법
+  // const rabbitRect = rabbit.getBoundingClientRect()
+  // const [x,y] = [rabbitRect.left, rabbitRect.top]
+  // const windowHeight = window.innerHeight;
 
-  const [targetX, targetY] = [0, parseInt(y - (windowHeight/2) + (rabbit.height)/2)]
-  console.log(targetX, targetY)
-  window.scrollTo(targetX, targetY)
+  // const [targetX, targetY] = [0, parseInt(y - (windowHeight/2) + (rabbit.height)/2)]
+  // console.log(targetX, targetY)
+  // window.scrollTo(targetX, targetY)
+
+  // 개선된 방법
+  rabbit.scrollIntoView({
+    behavior: "smooth",
+    // block: "center",
+    block: "nearest",
+  })
 })
