a967e73075022d53f1fe0fd65f14d9e6a671bf43
leejuhanKr <juhan96@gmail.com>
2022-06-01 23:40:26 +0900

    요소 이동 관련 성능 개선

--- coordinates/main.js
+++ coordinates/main.js
@@ -6,8 +6,9 @@ const coordinate = document.querySelector('.tag')
 targetMark.addEventListener('mousemove', event  =>{
     // console.log(event.clientX, event.clientY)
     const [x,y] = [event.clientX, event.clientY]
-    target.style.left = `${x}px`
-    target.style.top = `${y}px`
+    target.style.transform = `translate(${x}px, ${y}px)`
+    // target.style.left = `${x}px`
+    // target.style.top = `${y}px`
     coordinate.textContent = `${x}px, ${y}px`
 })
 
