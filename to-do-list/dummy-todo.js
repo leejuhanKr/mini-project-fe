457b765144023d67303296bb6bdfa4ad7404bced
leejuhanKr <juhan96@gmail.com>
2022-06-03 02:50:43 +0900

    test용 dummy todo data 생성

---
+++ to-do-list/dummy-todo.js
@@ -0,0 +1,19 @@
+// export default [
+const dummyData = 
+    [
+        {
+            createdAt:"",
+            todoText: "장보기",
+            isDone: false
+        },
+        {
+            createdAt:"",
+            todoText: "치과가기",
+            isDone: true
+        },
+        {
+            createdAt:"",
+            todoText: "공부하기",
+            isDone: false
+        },
+    ]
