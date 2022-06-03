3de531f33c110ae59a895a532f71f3f8275cdf8f
leejuhanKr <juhan96@gmail.com>
2022-06-03 23:52:27 +0900

    todo 완료 체크박스 구현

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -11,21 +11,30 @@ console.log(dummyData)
 const todoLists = document.querySelector(".todos")
 const inputBtn = document.querySelector(".add-btn")
 const deleteBtns = document.querySelectorAll(".delete-btn")
+const checkBtns = document.querySelectorAll(".check-btn")
 
 const makeNewTodo = (text)=> {
     // 추가할 list element 생성
     const listElement = document.createElement('li')
     listElement.className = "todo"
+
     // list에 들어갈 요소 생성
+    // todo contents
     const todoText = document.createElement('span')
     todoText.className = "todo-text"
     todoText.textContent = text
+    // todo trashcan
     const trashIcon = document.createElement('i')
     trashIcon.className = "delete-btn" 
     trashIcon.classList.add("fa-regular", "fa-trash-can")
     addDeleteEvent(trashIcon)  // trash click시 삭제 이벤트 추가
+    // todo checkbox
+    const checkIcon = document.createElement('i')
+    checkIcon.className = "check-btn"
+    checkIcon.classList.add("fa-regular", "fa-circle")
+    addCheckEvent(checkIcon)
     // list에 요소 추가
-    listElement.append(todoText, trashIcon)
+    listElement.append(checkIcon, todoText, trashIcon)
 
     return listElement
 }
@@ -39,8 +48,12 @@ dummyData.forEach(todoData => {
 // trash icon 클릭시 삭제 이벤트 추가
 deleteBtns.forEach(addDeleteEvent)
 
+// check icon 클릭시 이벤트 추가
+checkBtns.forEach(addCheckEvent)
+
 // add btn 클릭시 input내용 todo에 추가
 inputBtn.addEventListener('click', addTodotext)
+
 
 // enter누를 때 input내용 todo에 추가
 function addTodoWhenEnter() {
@@ -53,6 +66,7 @@ document.querySelector('.todo-inp').setAttribute("onkeyup","addTodoWhenEnter()")
 // add event callback
 function addTodotext(e) {
     const inputText = document.querySelector(".todo-inp")
+    if (inputText.value.length === 0) {return}
     todoLists.append(makeNewTodo(inputText.value)) //todo에 추가
     inputText.value = ""    // input내용 삭제    
 }
@@ -62,6 +76,18 @@ function addDeleteEvent(element) {
         console.log("clicked")
         const targetTodo = element.parentElement
         targetTodo.remove()
+    })
+}
+
+function addCheckEvent(element) {
+    element.addEventListener('click', event => {
+        const targetTodo = element.parentElement
+        const checkBtn = targetTodo.querySelector(".check-btn")
+        if (checkBtn.classList.contains("fa-circle")) {
+            checkBtn.classList.replace("fa-circle","fa-circle-check")
+        } else if (checkBtn.classList.contains("fa-circle-check")) {
+            checkBtn.classList.replace("fa-circle-check","fa-circle")
+        }
     })
 }
 
