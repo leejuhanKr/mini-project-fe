9dbef755fdb8b18a345fd86e57c5f4d1a6f2359f
leejuhanKr <juhan96@gmail.com>
2022-06-06 02:36:20 +0900

    delete기능, enter시 추가 기능 구현

--- to-do-list-clone/main.js
+++ to-do-list-clone/main.js
@@ -3,40 +3,52 @@ const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 
 function onAdd() {
-    // 1. 사용자가 입력한 텍스트를 받아옴
     const text = input.value;
     console.log(text);
-    // 2. 새로운 아이템을 만듦(텍스트 + 삭제 버튼)
+    if (text === "") {
+        input.focus();
+        return;
+    }
     const item = createItem(text);
-    // 3. items 컨테이너안에 새로 만든 아이템을 추가한다
     items.appendChild(item);
-    // 4. 인풋을 초기화 한다.
+    item.scrollIntoView({block: "center"})
     input.value = '';
     input.focus();
 }
 
 function createItem(text) {
+
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
 
     const item = document.createElement('div');
     item.setAttribute('class', 'item')
     
-    const span = document.createElement('span');
-    span.setAttribute('class', 'item__name');
-    span.innerText = text;
+    const name = document.createElement('span');
+    name.setAttribute('class', 'item__name');
+    name.innerText = text;
     
     const deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+    deleteBtn.addEventListener('click', ()=>{
+        items.removeChild(itemRow)
+    });
 
     const itemDivider = document.createElement('div');
     itemDivider.setAttribute('class', 'item__divider');
 
-    item.append(span)
-    itemRow.append(item)
+    item.append(name, deleteBtn)
+    itemRow.append(item, itemDivider)
     return itemRow
 }
 
 addBtn.addEventListener('click', () => {
     onAdd();
+})
+
+input.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        onAdd();
+    }
 })
